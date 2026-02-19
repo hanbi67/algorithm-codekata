673523ed6673f3b202858e9b4813f046d8661767
Lee Hanbi <hanbi6717@naver.com>
2026-02-19 09:58:39 +0900

    feat: 정수 제곱근 판별

--- src/Level2/Q19.java
+++ src/Level2/Q19.java
@@ -1,4 +1,15 @@
 package Level2;
-
+// 정수 제곱근 판별
 public class Q19 {
+    public long solution(long n) {
+        long answer = 0;
+        double x = Math.sqrt(n);
+        if(x == (int)x){
+            answer = (long)Math.pow(x+1, 2);
+        } else {
+            answer = -1;
+        }
+
+        return answer;
+    }
 }
